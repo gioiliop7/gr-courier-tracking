df1df6677f039640f2b774c036dba42bdbcd4eb4
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2022-07-09 13:31:47 +0300

    Fix english messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,82 @@ $(document).ready(function ($) {
         timeout: 0,
       };
 
+      function greeklishTranslate(s) {
+        var translateMap = {
+          ks: "ξ",
+          ps: "ψ",
+          th: "θ",
+          a: "α",
+          b: "β",
+          c: "σ",
+          d: "δ",
+          e: "ε",
+          f: "φ",
+          g: "γ",
+          h: "η",
+          i: "ι",
+          k: "κ",
+          l: "λ",
+          m: "μ",
+          n: "ν",
+          o: "ο",
+          p: "π",
+          q: "",
+          r: "ρ",
+          s: "σ",
+          t: "τ",
+          u: "υ",
+          v: "β",
+          w: "ω",
+          x: "χ",
+          y: "υ",
+          z: "ζ",
+          KS: "Ξ",
+          PS: "Ψ",
+          TH: "Θ",
+          A: "Α",
+          B: "Β",
+          C: "Σ",
+          D: "Δ",
+          E: "Ε",
+          F: "Φ",
+          G: "Γ",
+          H: "Η",
+          I: "Ί",
+          I: "Ι",
+          K: "Κ",
+          L: "Λ",
+          M: "Μ",
+          N: "Ν",
+          O: "Ο",
+          P: "Π",
+          R: "Ρ",
+          S: "Σ",
+          T: "Τ",
+          U: "Υ",
+          V: "Β",
+          W: "Ώ",
+          W: "Ω",
+          X: "Χ",
+          Y: "Υ",
+          Z: "Ζ",
+        };
+
+        var newText = "";
+        for (var x = 0; x < s.length; x++) {
+          if (translateMap[s.charAt(x - 1) + s.charAt(x)] && x > 0) {
+            //special case
+            newText = newText.substr(0, newText.length - 1);
+            newText +=
+              translateMap[s.charAt(x - 1) + s.charAt(x)] || s.charAt(x);
+          } else {
+            newText += translateMap[s.charAt(x)] || s.charAt(x);
+          }
+        }
+
+        return newText;
+      }
+
       function translateSkroutz(status) {
         switch (status) {
           case "Failed to pickup":
@@ -37,10 +113,36 @@ $(document).ready(function ($) {
         return status;
       }
 
+      function translateGeniki(status) {
+        switch (status) {
+          case "Shipment label created/printed":
+            status = "Δημιουργήθηκε/εκτυπώθηκε η ετικέτα αποστολής";
+            break;
+          case "Departure from drop off point":
+            status = "Αναχώρηση από το σημείο αποβίβασης";
+            break;
+          case "Assigned to courier":
+            status = "Ανατέθηκε σε courier";
+            break;
+          case "Departure from hub":
+            status = "Αναχώρηση από κόμβο διαλογής";
+            break;
+          case "Arrival at pick up point":
+            status = "Άφιξη στο σημείο παραλαβής";
+            break;
+          case "Delivery of shipment":
+            status = "Παράδοση αποστολής";
+            break;
+          default:
+            break;
+        }
+        return status;
+      }
+
       $.ajax(settings)
         .done(function (response) {
-          console.log(response);
           let found = response.found;
+          let strArray;
           if (found == true) {
             let courier_name = response.courier;
             let tracking_number = response.tracking_number;
@@ -48,7 +150,15 @@ $(document).ready(function ($) {
             let delivered = response.delivered;
             let last_update = response.last;
             let last_update_status = last_update.status;
-            last_update_status = translateSkroutz(last_update_status);
+            if (courier_name == "Skroutz Last Mile") {
+              last_update_status = translateSkroutz(last_update_status);
+            } else if (courier_name == "Geniki") {
+              last_update_status = translateGeniki(last_update_status);
+              if (last_update_status.includes("by")) {
+                strArray = last_update_status.split("by");
+                last_update_status = "Παραλήφθηκε από " + greeklishTranslate(strArray[1]);
+              }
+            }
             let space = last_update.space;
             let time = last_update.time;
             if (found == true) {
@@ -103,6 +213,11 @@ $(document).ready(function ($) {
                 let responses_time = responses[i].time;
                 let responses_status = responses[i].status;
                 responses_status = translateSkroutz(responses_status);
+                responses_status = translateGeniki(responses_status);
+                if (responses_status.includes("by")) {
+                  strArray = responses_status.split("by");
+                  responses_status = "Παραλήφθηκε από " + greeklishTranslate(strArray[1]);
+                }
                 let resposnes_space = responses[i].space;
                 const html_code = `<tr>
             <td>${responses_status}</td>
